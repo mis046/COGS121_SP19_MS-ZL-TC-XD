e4de968005dd6c7145c46bbe74ed4beefff44f25
ms <mis046@ucsd.edu>
2019-04-26 16:06:09 -0700

    index and sever

--- ms2_src_skeleton/sever.js
+++ ms2_src_skeleton/sever.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 // setup static file location, /static
-app.use(express.static('Static_files'));
+app.use(express.static('static_files'));
 
 // start sever at http://localhost:3000/
 app.listen(3000, () => {
-    console.log("Server start, localhost:3000");
+    console.log("Server starts, http://localhost:3000/");
 });
