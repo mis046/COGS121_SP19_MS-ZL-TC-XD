ef4da2261ce75e7323ec850cc5ecd819de152827
tyc980708 <1204025672@qq.com>
2019-04-26 18:08:08 -0700

    Update server.js

--- ms2_src_skeleton/server.js
+++ ms2_src_skeleton/server.js
@@ -1,29 +1,35 @@
 // initialize express
-const express = require('express');
+const express = require("express");
 const app = express();
-var path = require('path');
-
+var path = require("path");
 
 // setup static file location, /static
-app.use(express.static('static_files'));
+app.use(express.static("static_files"));
 
 // start sever at http://localhost:3000/
 app.listen(3000, () => {
-    console.log("Server starts, http://localhost:3000/");
+  console.log("Server starts, http://localhost:3000/");
 });
 
-app.get('/child_info', function (req, res) {
-    res.sendFile(path.join(__dirname + '/static_files/child_info.html'));
-})
+app.get("/child_info", function(req, res) {
+  res.sendFile(path.join(__dirname + "/static_files/child_info.html"));
+});
 
-app.get('/game_rec', function (req, res) {
-    res.sendFile(path.join(__dirname + '/static_files/game_rec.html'));
-})
+app.get("/game_rec", function(req, res) {
+  res.sendFile(path.join(__dirname + "/static_files/game_rec.html"));
+});
 
-app.get('/game_info', function (req, res) {
-    res.sendFile(path.join(__dirname + '/static_files/game_info.html'));
-})
+app.get("/game_info", function(req, res) {
+  res.sendFile(path.join(__dirname + "/static_files/game_info.html"));
+});
 
-app.get('/buy', function (req, res) {
-    res.sendFile(path.join(__dirname + '/static_files/buy.html'));
-})
+app.get("/buy", function(req, res) {
+  res.sendFile(path.join(__dirname + "/static_files/buy.html"));
+});
+
+//fakeDB
+const fakeDB = {
+  Pokemon: { age: 10, rating: 8 },
+  NBA2k: { age: 13, rating: 8.5 },
+  Sekiro: { age: 17, rating: 9.5 }
+};
