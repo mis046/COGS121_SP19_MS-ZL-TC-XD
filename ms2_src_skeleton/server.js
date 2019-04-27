8bfb52fac6e7a9407c96aba19edfa8dfe23c0205
tyc980708 <1204025672@qq.com>
2019-04-26 18:57:56 -0700

    ajax added

--- ms2_src_skeleton/server.js
+++ ms2_src_skeleton/server.js
@@ -11,6 +11,16 @@ app.listen(3000, () => {
   console.log("Server starts, http://localhost:3000/");
 });
 
+app.get('/games', (req, res) => {
+  const game_names = Object.keys(fakeDB);
+  res.send(game_names);
+});
+
+app.get('/game_rec', (req,res) => {
+  const game1 = Object.keys(fakeDB);
+  res.send(game1);
+});
+
 //fakeDB
 const fakeDB = {
   Pokemon: { age: 10, rating: 8 },
