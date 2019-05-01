99b24c9d52df82d4e583dd803628b6c6322a2847
ms <mis046@ucsd.edu>
2019-04-30 19:50:39 -0700

    cover

--- ms2_src_skeleton/server.js
+++ ms2_src_skeleton/server.js
@@ -13,17 +13,17 @@ app.listen(3000, () => {
 
 app.get('/games', (req, res) => {
   const game_names = Object.keys(fakeDB);
-  res.send(game_names);
+  res.send(fakeDB);
 });
 
 app.get('/game_rec', (req,res) => {
   const game1 = Object.keys(fakeDB);
-  res.send(game1);
+  res.send(fakeDB);
 });
 
 //fakeDB
 const fakeDB = {
-  Pokemon: { age: 10, rating: 8 },
-  NBA2k: { age: 13, rating: 8.5 },
-  Sekiro: { age: 17, rating: 9.5 }
+  Pokemon: { age: 10, rating: 8, coverPic: "picture/game_pokemon.jpg"},
+  NBA2k: { age: 13, rating: 8.5, coverPic: "picture/game_nba2k.jpg"},
+  Sekiro: { age: 17, rating: 9.5, coverPic: "picture/game_sekiro.jpg"}
 };
