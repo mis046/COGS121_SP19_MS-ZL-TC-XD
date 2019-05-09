74ee2fe06bf994796f749552d2fc9dba9a3af4b6
Allen <zhl270@ucsd.edu>
2019-05-09 12:55:15 -0700

    able to read api data

--- ms3_src/server.js
+++ ms3_src/server.js
@@ -1,7 +1,13 @@
 // initialize express
 const express = require("express");
 const app = express();
+const axios = require('axios');
 var path = require("path");
+
+let childInfoQuery = "";
+let filterQuery = ""; // games that already been purchased
+let childInfo = {};
+
 
 // setup static file location, /static
 app.use(express.static("static_files"));
@@ -17,8 +23,42 @@ app.get('/games', (req, res) => {
 });
 
 app.get('/game_rec', (req,res) => {
-  const game1 = Object.keys(fakeDB);
-  res.send(fakeDB);
+  let result = "result";
+  axios({
+    url: "https://api-v3.igdb.com/games",
+    method: 'GET',
+    headers: {
+        'user-key': 'd90ddec63ca35266adbb5eee86e1b822'
+    },
+    data: "sort aggregated_rating desc;fields name,genres.name,aggregated_rating,age_ratings.rating,storyline,summary,cover.*;" 
+    + "where aggregated_rating != null & rating_count > 50 " + childInfoQuery + "; limit 1;"
+  })
+    .then(response => {
+        result = response.data[0]; // object result 
+        //console.log(result);
+        res.send(result);   
+    })
+    .catch(err => {
+        console.error(err);
+    });
+  //const game1 = Object.keys(fakeDB);
+  //res.send(result);
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true})); // hook up with your app
+app.post('/game_rec', (req,res) => {
+  childInfo = req.body;
+  let genreQuery = " (";
+  for(const genre of childInfo.genres) {
+    genreQuery = genreQuery + ' (genres.name = "' + genre + '") |';
+  }
+  genreQuery = genreQuery.slice(0, -1);
+  genreQuery = genreQuery + ")";
+  childInfoQuery = "" + "& platforms = " + childInfo.platform + " &" + genreQuery;
+  console.log(childInfoQuery);
+  res.send("success");
+  // STORE THE DATA TO DB 
 });
 
 //fakeDB
