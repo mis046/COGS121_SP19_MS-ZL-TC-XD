bd72686b52f0934a3a0e719968c1d8e7315ae4b5
ms <mis046@ucsd.edu>
2019-05-11 15:55:00 -0700

    fix

--- ms3_src/server.js
+++ ms3_src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const axios = require("axios");
 var path = require("path");
 
-// initialize firebase db 
+// initialize firebase db
 var firebaseConfig = {
   apiKey: "AIzaSyApxM5dVTvRHjfqNfD5xygT03JzTdyHBhs",
   authDomain: "cogs121proj.firebaseapp.com",
@@ -14,11 +14,10 @@ var firebaseConfig = {
   messagingSenderId: "75409493864",
   appId: "1:75409493864:web:03c5b8c4be96c7db"
 };
-var firebase = require('firebase/app');
-require('firebase/database');
+var firebase = require("firebase/app");
+require("firebase/database");
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-
 
 let childInfoQuery = "";
 let filterQuery = ""; // games that already been purchased
@@ -44,17 +43,19 @@ app.get("/login", (req, res) => {
 app.get("/game_rec", (req, res) => {
   let result = "result";
 
-  // build up filter game query 
+  // build up filter game query
   // current game have a delay to appear on db, so need to be added manually
   let currGame = req.query.name;
-  let query = firebase.database().ref(currUser.ID +  "/").orderByKey();
-  query.once("value")
-    .then(function(snapshot) {
-      filterQuery = "";
-      snapshot.child("library").forEach(function(childSnapshot) {
-        // key will be "ada" the first time and "alan" the second time
-        let filter_name = childSnapshot.val().name;
-        filterQuery += ' & name != "' + filter_name + '"';
+  let query = firebase
+    .database()
+    .ref(currUser.ID + "/")
+    .orderByKey();
+  query.once("value").then(function(snapshot) {
+    filterQuery = "";
+    snapshot.child("library").forEach(function(childSnapshot) {
+      // key will be "ada" the first time and "alan" the second time
+      let filter_name = childSnapshot.val().name;
+      filterQuery += ' & name != "' + filter_name + '"';
     });
   });
   filterQuery += ' & name != "' + currGame + '"';
@@ -62,7 +63,7 @@ app.get("/game_rec", (req, res) => {
   // TODO: 1. add age filter
   // IGDB [0-5: 2,    6-9: 2/3;    10-12: 2/3/4,    13-16: 2/3/4/5,    17: 2/3/4/5/6,    18: 2/3/4/5/6/7]+5
   // TODO: 2. can retrieve more game details to pass to game_info.html
-  // TODO: 3. try to modify the query to get more games, current query will only return around 
+  // TODO: 3. try to modify the query to get more games, current query will only return around
   // 30 games or less.
   axios({
     url: "https://api-v3.igdb.com/games",
@@ -73,7 +74,8 @@ app.get("/game_rec", (req, res) => {
     data:
       "sort aggregated_rating desc;fields name,genres.name,aggregated_rating,age_ratings.rating,storyline,summary,cover.*;" +
       "where aggregated_rating != null" +
-      childInfoQuery + filterQuery +
+      childInfoQuery +
+      filterQuery +
       "; limit 1;"
   })
     .then(response => {
@@ -86,9 +88,8 @@ app.get("/game_rec", (req, res) => {
     });
 });
 
-// TODO: automatically fill the child info section, pass to child_info 
-app.get("/child_info", (req, res) => {
-});
+// TODO: automatically fill the child info section, pass to child_info
+app.get("/child_info", (req, res) => {});
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true })); // hook up with your app
@@ -100,22 +101,17 @@ app.post("/child_info", (req, res) => {
   // make a age query base on age
   let age = childInfo.age;
   if (age >= 18) {
-    ageQuery += "(7,8,9,10,11,12) )"
-  }
-  else if (age >= 17) {
-    ageQuery += "(7,8,9,10,11) )"
-  }
-  else if (age >= 13) {
-    ageQuery += "(7,8,9,10) )"
-  }
-  else if (age >= 10) {
-    ageQuery += "(7,8,9) )"
-  }
-  else if (age >= 6) {
-    ageQuery += "(7,8) )"
-  }
-  else {
-    ageQuery += "(7) )"
+    ageQuery += "(7,8,9,10,11,12) )";
+  } else if (age >= 17) {
+    ageQuery += "(7,8,9,10,11) )";
+  } else if (age >= 13) {
+    ageQuery += "(7,8,9,10) )";
+  } else if (age >= 10) {
+    ageQuery += "(7,8,9) )";
+  } else if (age >= 6) {
+    ageQuery += "(7,8) )";
+  } else {
+    ageQuery += "(7) )";
   }
 
   let genreQuery = " (";
@@ -125,7 +121,13 @@ app.post("/child_info", (req, res) => {
   genreQuery = genreQuery.slice(0, -1);
   genreQuery = genreQuery + ")";
   childInfoQuery =
-    "" + "& platforms = " + childInfo.platform + " &" + genreQuery + " &" + ageQuery;
+    "" +
+    "& platforms = " +
+    childInfo.platform +
+    " &" +
+    genreQuery +
+    " &" +
+    ageQuery;
   res.send("successlly get child info");
 });
 app.post("/login", (req, res) => {
